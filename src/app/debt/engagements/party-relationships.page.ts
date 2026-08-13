ea13fafb291bdd652a3c81ae17b644da4e71ce10
Phil Armstrong <phil.armstrong@itv.com>
2026-08-13 15:50:02 +0100

    delete accidental code

--- src/app/debt/engagements/party-relationships.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-party-relationships-page',
-  imports: [],
-  templateUrl: './party-relationships.page.html',
-  styleUrl: './party-relationships.page.css',
-})
-export class PartyRelationshipsPage {}
